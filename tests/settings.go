5baedaed952761a50626426070d5903add90f3b9
Evgeny Zentsov <47511832+mnemonik79@users.noreply.github.com>
2024-11-25 11:01:11 +0700

    Add files via upload

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiNTk5NDQ3MWFiYjAxMTEyYWZjYzE4MTU5ZjZjYzc0YjRmNTExYjk5ODA2ZGE1OWIzY2FmNWE5YzE3M2NhY2ZjNSIsImV4cCI6MTcyMDA0MTkxMX0.H2CFNh4CyzfD2nWlWq5vLShwAlrzBpbuEgYIhV2P7go`
