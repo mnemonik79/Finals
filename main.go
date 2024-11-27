9b11fd841891e56cb9d5ddcac21382dcf3817b2d
Evgeny Zentsov <47511832+mnemonik79@users.noreply.github.com>
2024-11-27 12:57:08 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"ServerFinal/internal/authtentification"
-	"ServerFinal/internal/database"
-	"ServerFinal/internal/handlers"
-	"ServerFinal/internal/settings"
-	"ServerFinal/internal/store"
+	"os"
 
 	"github.com/go-chi/chi"
+
+	auth "github.com/mnemonik79/Finals/internal/authtentification"
+	"github.com/mnemonik79/Finals/internal/database"
+	"github.com/mnemonik79/Finals/internal/handlers"
+	"github.com/mnemonik79/Finals/internal/settings"
+	"github.com/mnemonik79/Finals/internal/store"
 )
 
 func main() {
@@ -21,17 +24,22 @@ func main() {
 	r := chi.NewRouter()
 	r.Handle("/*", http.FileServer(http.Dir("./web")))
 	r.Get("/api/nextdate", handlers.HandleNextDate)
-	r.Post("/api/task", authtentification.Authentification(handlers.HandlePostGetPutRequests(store)))
-	r.Get("/api/tasks", authtentification.Authentification(handlers.HandleTasksGet(store)))
+	r.Post("/api/task", auth.Authentification(handlers.HandlePostGetPutRequests(store)))
+	r.Get("/api/tasks", auth.Authentification(handlers.HandleTasksGet(store)))
 	r.Get("/api/task", handlers.HandlePostGetPutRequests(store))
 	r.Put("/api/task", handlers.HandlePostGetPutRequests(store))
-	r.Post("/api/task/done", authtentification.Authentification(handlers.HandleTaskDone(store)))
+	r.Post("/api/task/done", auth.Authentification(handlers.HandleTaskDone(store)))
 	r.Delete("/api/task", handlers.HandlePostGetPutRequests(store))
-	r.Post("/api/signin", authtentification.HandleSiginingIn)
+	r.Post("/api/signin", auth.HandleSiginingIn)
 
 	environment := settings.GetEnv()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
 	err := http.ListenAndServe(":"+environment.Port, r)
 	if err != nil {
 		fmt.Println("Не удалось запустить сервер:\n", err)
+	} else {
+		infoLog.Print("Запуск сервера")
 	}
 }
