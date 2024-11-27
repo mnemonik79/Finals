ef046a6c6ad525003b68dbeae5b92828ac3245fc
Evgeny Zentsov <47511832+mnemonik79@users.noreply.github.com>
2024-11-27 12:44:17 +0700

    Add files via upload

--- internal/database/database.go
+++ internal/database/database.go
@@ -15,7 +15,7 @@ func InitializeDatabase() *sql.DB {
 		log.Fatal(err)
 	}
 
-	dbFile := filepath.Join(filepath.Dir(appPath), "ServerFinal", "scheduler.db")
+	dbFile := filepath.Join(filepath.Dir(appPath), "Finals", "scheduler.db")
 	environment := os.Getenv("TODO_DBFILE")
 	if len(environment) > 0 {
 		dbFile = environment
